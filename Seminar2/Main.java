f8cafa722f33d9d7deecdc0bcaa46555e7da191f
aditzu26 <126081000+aditzu26@users.noreply.github.com>
2024-02-27 23:58:21 +0200

    Add files via upload

---
+++ Seminar2/Main.java
@@ -0,0 +1,65 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+/*
+ interface vehicle {
+}
+ class PrintDetails {
+    public void printDetails(){
+
+    }
+}
+
+class ValueCalculation{
+    public double calculateValue(){
+        return calculateValue();
+    }
+}
+
+class VehicleToDB{
+    public void addVehicletoDB(){
+
+    }
+}
+*/
+////////////////////////////////////////////
+/*
+ interface vehicle1{
+    public double calculateValue();
+}
+
+class Car implements vehicle1{
+    @Override
+    public double calculateValue(){
+     return calculateValue() * 0.8;
+    }
+}
+
+class Bike implements vehicle1{
+    @Override
+    public double calculateValue(){
+        return calculateValue() * 0.5;
+    }
+}
+*/
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/*
+interface heightable{
+
+    public void setHeight(double h);
+
+}
+
+interface widthable{
+    public void setWidth(double W);
+}
+
+class Rectangle implements heightable{
+    @Override
+    public void setHeight(double h){}
+
+
+}
+
+class Square implements widthable{
+    public void setWidth(double W){}
+}*/
